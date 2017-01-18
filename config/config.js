63f9229156cd786827112184812529db4f65c9dc
shazw89 <shyan.wong@gmail.com>
2017-01-18 10:31:35 +0000

    sorted out the info window

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  databaseURL: process.env.MONGODB_URI || 'mongodb://localhost:27017/islands-app',
+  db: process.env.MONGODB_URI || 'mongodb://localhost/islands-app',
   secret: process.env.SECRET || 'Come hide away here!'
 };
